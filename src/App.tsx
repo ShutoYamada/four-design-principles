9c7a5e1ec58228898fbcfe479e38aeca4054c41d
ShutoYamada <stym1122@gmail.com>
2021-03-31 17:03:33 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,188 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  // 近接
+  const [isNear, setNear] = useState(false);
+  // 整列
+  const [isFormat, setFormat] = useState(false);
+  // 反復
+  const [isRepeat, setRepeat] = useState(false);
+  // 強調
+  const [isStrong, setStrong] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Shuto Yamadaのおすすめミステリ作家/本</h1>
+        <div
+          style={{
+            marginTop: isNear ? 12 : 2,
+            marginBottom: isNear ? 12 : 2,
+            width: isFormat ? "55%" : "auto",
+          }}
         >
-          Learn React
-        </a>
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              fontSize: isStrong ? 20 : 16,
+              fontWeight: isStrong ? "bold" : "normal",
+              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+            }}
+          >
+            {`${isRepeat ? "●" : "★"}綾辻行人`}
+          </p>
+          <p style={{ margin: 0, textAlign: "left" }}>
+            十角館の殺人、時計館の殺人、Another
+          </p>
+        </div>
+        <div
+          style={{
+            marginTop: isNear ? 12 : 2,
+            marginBottom: isNear ? 12 : 2,
+            width: isFormat ? "55%" : "auto",
+          }}
+        >
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              textDecoration: isRepeat ? "none" : "underline",
+              fontSize: isStrong ? 20 : 16,
+              fontWeight: isStrong ? "bold" : "normal",
+              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+            }}
+          >
+            {`${isRepeat ? "●" : "☆"}有栖川有栖`}
+          </p>
+          <p style={{ margin: 0, textAlign: "left" }}>
+            スイス時計の謎、月光ゲーム、双頭の悪魔
+          </p>
+        </div>
+        <div
+          style={{
+            marginTop: isNear ? 12 : 2,
+            marginBottom: isNear ? 12 : 2,
+            width: isFormat ? "55%" : "auto",
+          }}
+        >
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              color: isRepeat ? "white" : "yellow",
+              fontSize: isStrong ? 20 : 16,
+              fontWeight: isStrong ? "bold" : "normal",
+              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+            }}
+          >
+            {`${isRepeat ? "●" : "◯"}麻耶雄嵩`}
+          </p>
+          <p style={{ margin: 0, textAlign: "left" }}>
+            翼ある闇、蛍、神様ゲーム
+          </p>
+        </div>
+        <div
+          style={{
+            marginTop: isNear ? 12 : 2,
+            marginBottom: isNear ? 12 : 2,
+            width: isFormat ? "55%" : "auto",
+          }}
+        >
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              fontSize: isStrong ? 20 : 16,
+              fontWeight: isStrong ? "bold" : "normal",
+              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+            }}
+          >
+            {`${isRepeat ? "●" : "■"}歌野晶午`}
+          </p>
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              color: isRepeat ? "white" : "green",
+            }}
+          >
+            密室殺人ゲーム、葉桜の季節に君を想うということ
+          </p>
+        </div>
+        <div
+          style={{
+            marginTop: 12,
+            marginBottom: 12,
+            width: isFormat ? "55%" : "auto",
+          }}
+        >
+          <p
+            style={{
+              margin: 0,
+              textAlign: "left",
+              fontSize: isStrong ? 20 : 16,
+              fontWeight: isStrong ? "bold" : "normal",
+              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+            }}
+          >
+            {`${isRepeat ? "●" : "□"}東野圭吾`}
+          </p>
+          <p style={{ margin: 0, textAlign: "left" }}>
+            秘密、容疑者Xの献身、どちらかが彼女を殺した
+          </p>
+        </div>
+        <h1>デザイン基本4原則のパターンを当てはめる</h1>
+        <form>
+          <label style={{ margin: 10 }}>
+            近接:
+            <input
+              type="checkBox"
+              checked={isNear}
+              name="near"
+              onChange={(e) => {
+                setNear(e.target.checked);
+              }}
+            />
+          </label>
+          <label style={{ margin: 10 }}>
+            整列:
+            <input
+              type="checkBox"
+              checked={isFormat}
+              name="format"
+              onChange={(e) => {
+                setFormat(e.target.checked);
+              }}
+            />
+          </label>
+          <label style={{ margin: 10 }}>
+            反復:
+            <input
+              type="checkBox"
+              checked={isRepeat}
+              name="repeat"
+              onChange={(e) => {
+                setRepeat(e.target.checked);
+              }}
+            />
+          </label>
+          <label style={{ margin: 10 }}>
+            強調:
+            <input
+              type="checkBox"
+              checked={isStrong}
+              name="strong"
+              onChange={(e) => {
+                setStrong(e.target.checked);
+              }}
+            />
+          </label>
+        </form>
       </header>
     </div>
   );
-}
+};
 
 export default App;
