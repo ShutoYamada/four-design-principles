165a9fbda2a7f074cd00acafb69314d54a4e8fc0
ShutoYamada <stym1122@gmail.com>
2021-04-01 11:20:01 +0900

    整列のスタイルを分かりやすい者に変更

--- src/App.tsx
+++ src/App.tsx
@@ -15,123 +15,125 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <h1>Shuto Yamadaのおすすめミステリ作家/本</h1>
-        <div
-          style={{
-            marginTop: isNear ? 12 : 2,
-            marginBottom: isNear ? 12 : 2,
-            width: isFormat ? "55%" : "auto",
-          }}
-        >
-          <p
+        <div>
+          <div
             style={{
-              margin: 0,
-              textAlign: "left",
-              fontSize: isStrong ? 20 : 16,
-              fontWeight: isStrong ? "bold" : "normal",
-              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              marginTop: isNear ? 12 : 2,
+              marginBottom: isNear ? 12 : 2,
             }}
           >
-            {`${isRepeat ? "●" : "★"}綾辻行人`}
-          </p>
-          <p style={{ margin: 0, textAlign: "left" }}>
-            十角館の殺人、時計館の殺人、Another
-          </p>
-        </div>
-        <div
-          style={{
-            marginTop: isNear ? 12 : 2,
-            marginBottom: isNear ? 12 : 2,
-            width: isFormat ? "55%" : "auto",
-          }}
-        >
-          <p
+            <p
+              style={{
+                margin: 0,
+                textAlign: isFormat ? "left" : "center",
+                fontSize: isStrong ? 20 : 16,
+                fontWeight: isStrong ? "bold" : "normal",
+                backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              }}
+            >
+              {`${isRepeat ? "●" : "★"}綾辻行人`}
+            </p>
+            <p
+              style={{
+                margin: 0,
+                textAlign: "left",
+              }}
+            >
+              十角館の殺人、時計館の殺人、Another
+            </p>
+          </div>
+          <div
             style={{
-              margin: 0,
-              textAlign: "left",
-              textDecoration: isRepeat ? "none" : "underline",
-              fontSize: isStrong ? 20 : 16,
-              fontWeight: isStrong ? "bold" : "normal",
-              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              marginTop: isNear ? 12 : 2,
+              marginBottom: isNear ? 12 : 2,
             }}
           >
-            {`${isRepeat ? "●" : "☆"}有栖川有栖`}
-          </p>
-          <p style={{ margin: 0, textAlign: "left" }}>
-            スイス時計の謎、月光ゲーム、双頭の悪魔
-          </p>
-        </div>
-        <div
-          style={{
-            marginTop: isNear ? 12 : 2,
-            marginBottom: isNear ? 12 : 2,
-            width: isFormat ? "55%" : "auto",
-          }}
-        >
-          <p
+            <p
+              style={{
+                margin: 0,
+                textAlign: isFormat ? "left" : "center",
+                textDecoration: isRepeat ? "none" : "underline",
+                fontSize: isStrong ? 20 : 16,
+                fontWeight: isStrong ? "bold" : "normal",
+                backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              }}
+            >
+              {`${isRepeat ? "●" : "☆"}有栖川有栖`}
+            </p>
+            <p style={{ margin: 0, textAlign: "left" }}>
+              スイス時計の謎、月光ゲーム、双頭の悪魔
+            </p>
+          </div>
+          <div
             style={{
-              margin: 0,
-              textAlign: "left",
-              color: isRepeat ? "white" : "yellow",
-              fontSize: isStrong ? 20 : 16,
-              fontWeight: isStrong ? "bold" : "normal",
-              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              marginTop: isNear ? 12 : 2,
+              marginBottom: isNear ? 12 : 2,
             }}
           >
-            {`${isRepeat ? "●" : "◯"}麻耶雄嵩`}
-          </p>
-          <p style={{ margin: 0, textAlign: "left" }}>
-            翼ある闇、蛍、神様ゲーム
-          </p>
-        </div>
-        <div
-          style={{
-            marginTop: isNear ? 12 : 2,
-            marginBottom: isNear ? 12 : 2,
-            width: isFormat ? "55%" : "auto",
-          }}
-        >
-          <p
+            <p
+              style={{
+                margin: 0,
+                textAlign: isFormat ? "left" : "center",
+                color: isRepeat ? "white" : "yellow",
+                fontSize: isStrong ? 20 : 16,
+                fontWeight: isStrong ? "bold" : "normal",
+                backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              }}
+            >
+              {`${isRepeat ? "●" : "◯"}麻耶雄嵩`}
+            </p>
+            <p style={{ margin: 0, textAlign: "left" }}>
+              翼ある闇、蛍、神様ゲーム
+            </p>
+          </div>
+          <div
             style={{
-              margin: 0,
-              textAlign: "left",
-              fontSize: isStrong ? 20 : 16,
-              fontWeight: isStrong ? "bold" : "normal",
-              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              marginTop: isNear ? 12 : 2,
+              marginBottom: isNear ? 12 : 2,
             }}
           >
-            {`${isRepeat ? "●" : "■"}歌野晶午`}
-          </p>
-          <p
+            <p
+              style={{
+                margin: 0,
+                textAlign: isFormat ? "left" : "center",
+                fontSize: isStrong ? 20 : 16,
+                fontWeight: isStrong ? "bold" : "normal",
+                backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              }}
+            >
+              {`${isRepeat ? "●" : "■"}歌野晶午`}
+            </p>
+            <p
+              style={{
+                margin: 0,
+                textAlign: "left",
+                color: isRepeat ? "white" : "green",
+              }}
+            >
+              密室殺人ゲーム、葉桜の季節に君を想うということ
+            </p>
+          </div>
+          <div
             style={{
-              margin: 0,
-              textAlign: "left",
-              color: isRepeat ? "white" : "green",
+              marginTop: 12,
+              marginBottom: 12,
             }}
           >
-            密室殺人ゲーム、葉桜の季節に君を想うということ
-          </p>
-        </div>
-        <div
-          style={{
-            marginTop: 12,
-            marginBottom: 12,
-            width: isFormat ? "55%" : "auto",
-          }}
-        >
-          <p
-            style={{
-              margin: 0,
-              textAlign: "left",
-              fontSize: isStrong ? 20 : 16,
-              fontWeight: isStrong ? "bold" : "normal",
-              backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
-            }}
-          >
-            {`${isRepeat ? "●" : "□"}東野圭吾`}
-          </p>
-          <p style={{ margin: 0, textAlign: "left" }}>
-            秘密、容疑者Xの献身、どちらかが彼女を殺した
-          </p>
+            <p
+              style={{
+                margin: 0,
+                textAlign: isFormat ? "left" : "center",
+                fontSize: isStrong ? 20 : 16,
+                fontWeight: isStrong ? "bold" : "normal",
+                backgroundColor: isStrong ? "rgb(255,139,80)" : "transparent",
+              }}
+            >
+              {`${isRepeat ? "●" : "□"}東野圭吾`}
+            </p>
+            <p style={{ margin: 0, textAlign: "left" }}>
+              秘密、容疑者Xの献身、どちらかが彼女を殺した
+            </p>
+          </div>
         </div>
         <h1>デザイン基本4原則のパターンを当てはめる</h1>
         <form>
